98d05ccfd49a4133b3d760c11dcc3d8bb50650d6
domaradzkiJs <domaradzkicode@gmail.com>
2019-10-15 11:05:30 +0200

    Updates

---
+++ precache-manifest.e95ed82f09b5bb4af5111f7d51a658b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6159ccef34e7a429ffc1e7ab78fad444",
+    "url": "/my-site/build/index.html"
+  },
+  {
+    "revision": "6cb41dca77829f335b74",
+    "url": "/my-site/build/static/css/main.8015ac40.chunk.css"
+  },
+  {
+    "revision": "f85910e2e4c918a43afb",
+    "url": "/my-site/build/static/js/2.0750ee86.chunk.js"
+  },
+  {
+    "revision": "6cb41dca77829f335b74",
+    "url": "/my-site/build/static/js/main.8a13f867.chunk.js"
+  },
+  {
+    "revision": "42017652b7a86fe34ea7",
+    "url": "/my-site/build/static/js/runtime-main.8df4828a.js"
+  },
+  {
+    "revision": "723daae2577bfd7b73e0334adafdacdf",
+    "url": "/my-site/build/static/media/github.723daae2.png"
+  },
+  {
+    "revision": "f4f8fd1de71d95b361494ef2f11fbcc9",
+    "url": "/my-site/build/static/media/mail.f4f8fd1d.png"
+  },
+  {
+    "revision": "4d9033cb1ce8088459e28767082242d5",
+    "url": "/my-site/build/static/media/phone.4d9033cb.png"
+  }
+]);
